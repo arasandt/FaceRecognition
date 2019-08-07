adabb3cee44b78df767df13ea2580c7efa92c0b7
Thiru Arasan Dayalan <ThiruArasan.Dayalan@cognizant.com>
2019-08-07 16:53:21 +0530

    removed mtcnn and starting with unknown tracking

--- main_azure.py
+++ main_azure.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
-
 from __future__ import print_function
+
+import random
 
 
 import cognitive_face as CF
@@ -38,8 +39,10 @@ hits_required =  hits_window_size * 0.20 #(20% of window size should have good h
 cool_time = timedelta(seconds=10)
 retention_time = timedelta(seconds=5)
 label_size = 150
-picture_size = 150
+picture_size = 100
 decision_size = 50
+each_row_size = 100
+unknown_size = 100
 
 def check_auth(label_id, office, floor):
     label_id = str(label_id)
@@ -189,8 +192,8 @@ def process_video_feed(filename):
     count = 0
     
     person_detail = {}
-    from mtcnn.mtcnn import MTCNN
-    detector = MTCNN()
+    #from mtcnn.mtcnn import MTCNN
+    #detector = MTCNN()
     
     length = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
     fps = int(video.get(cv2.CAP_PROP_FPS))
@@ -198,8 +201,10 @@ def process_video_feed(filename):
     frame_height = int(video.get(4))
     
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    vout = cv2.VideoWriter(filename + '.output.avi', fourcc, fps, (frame_width + label_size + picture_size + decision_size, frame_height))
+    vout = cv2.VideoWriter(filename + '.output.avi', fourcc, fps, (frame_width + label_size + picture_size + decision_size + unknown_size, frame_height))
+    
 
+    random.seed(9001)
     
     while True:
         (ret, frame) = video.read()
@@ -230,189 +235,190 @@ def process_video_feed(filename):
         #bb_box = detector.detect_faces(frame)
         #bb_box = [i for i in bb_box if i['confidence'] >= 0.9 ]
         #print(bb_box)
-        print(res)
-        count += 1
-        continue
-            
-        nrof_faces = len(bb_box)
+        #print(res)
+        face_ids = {d['faceId']:d['faceRectangle'] for d in res}
+        nrof_faces = len(face_ids.keys())
+        #print(face_ids)
+        personIds = CF.person.lists(person_group_id)
+        personId = {person['personId']: person["name"] for person in personIds}  
+
+
+        def getRectangle(rect, shp,percentage=0.25):#faceDictionary):
+            #rect = faceDictionary['faceRectangle']
+            wpadding = int(rect['width'] * percentage) # 25% increase
+            hpadding = int(rect['height'] * percentage)
         
-        face_box = []
+            det = [0,0,0,0]
+            det[0] = max(rect['left'] - wpadding, 0)
+            det[1] = max(rect['top'] - hpadding,0)
+            det[2] = min(rect['height'] + rect['left'] + wpadding,shp[1])
+            det[3] = min(rect['width'] + rect['top'] + hpadding,shp[0])
+
+            return det
+
+    
+
         cropped_image = []
+        face_box = []
         
-        if nrof_faces > 0:
-            for i in range(nrof_faces):
-                bb = bb_box[i]['box']
-                
-                #det = expand_bb(bb, frame.shape, percentage=0)
-                
-                #orig_image = frame[det[1]:det[3], det[0]:det[2]].copy()
-
-                #det = expand_bb(bb, frame.shape, percentage=0.25)
-                det = expand_bb(bb, frame.shape, percentage=0.50)
-                
-                
-                cropped_image.append(frame[det[1]:det[3], det[0]:det[2]].copy())
-                #file = 'temp.jpg'
-                #cv2.imwrite(file,cropped_image[-1])
-
-                personIds = CF.person.lists(person_group_id)
-                personId = {person['personId']: person["name"] for person in personIds}                
-
-                res = CF.face.detect(file)
-                #print(res)
-                face_ids = [d['faceId'] for d in res]
+        if face_ids:
+            face_res = CF.face.identify(list(face_ids.keys()),person_group_id)
+            #print(face_res)
+            for i, f in enumerate(face_ids.keys()):
                 label_id = None
-                confidence = 0.0
+                confidence = 0
+                candidates = {x['personId']:x['confidence'] for x in face_res[i]['candidates']}
+                #print(candidates)
+                if candidates:
+                    max_candidates = max(candidates.items(), key=operator.itemgetter(1))[0]
+                    if candidates[max_candidates] > 0.50 :
+                        label_id = personId[max_candidates]
+                        confidence = candidates[max_candidates]            
+                #det = expand_bb(face_ids[f], frame.shape, percentage=0.25)
+                #det = face_ids[f]
+                #cropped_image.append(frame[det[1]:det[3], det[0]:det[2]].copy())
+                #face_box.append((det,label_id, confidence))
                 
-                if face_ids:
-                    res = CF.face.identify(face_ids,person_group_id)    
-                    #print(res)
-                    candidates = {i['personId']:i['confidence'] for i in res[0]['candidates']}
-                    #print(candidates)
-                    if candidates:
-                        max_candidates = max(candidates.items(), key=operator.itemgetter(1))[0]
-                        #print(personId[max_candidates], candidates[max_candidates])
-                        if candidates[max_candidates] > 0.50 :
-                            label_id = personId[max_candidates]
-                            confidence = candidates[max_candidates]
-                    #cv2.rectangle(frame, (det[0], det[1]), (det[2], det[3]), (0, 255, 0), 2)
-                    face_box.append((det,label_id, confidence))
-                #cropped_images.append(frame[bb[1]:bb[1]+bb`[3], bb[0]:bb[0]+bb[2]].copy())
-                #cv2.rectangle(frame, (max(bb[0]-wpadding,0), max(bb[1]-hpadding,0)), (min(bb[0] + bb[2] + wpadding,frame.shape[1]), min(bb[1] + bb[3] + hpadding,frame.shape[0])), (0, 255, 0), 2)
-                
-                #print(orig_image.shape)
-                #orig_image = cv2.resize(orig_image,(160,160))
-                #print(orig_image.shape)
-                #cv2.imshow('person',orig_image)
-                #cv2.imwrite(str(count) + str(i) + '.jpg', orig_image)
-                #label_id = recognize_face(orig_image, i)
-                # get label of persons identified and details
-                
-                #time.sleep(10)
-            
-                
-            
-                #######label_id = random.randint(128537,128538)
+                det = getRectangle(face_ids[f],frame.shape, percentage=0.5)
+                cropped_image.append(frame[det[1]:det[3], det[0]:det[2]].copy())
+                #print(det, frame.shape)
+                face_box.append((det,label_id, confidence))
+                #cv2.rectangle(frame, (det[0], det[1]), (det[2], det[3]), (0,255,0), 2)
                 if label_id is not None:
                     if person_detail.get(label_id, 0) == 0:
                         person_detail[label_id] = Person_Details(label_id)
                     
-                    #person_detail[label_id].increment_count()
-                    person_detail[label_id].assign_face(cropped_image[i], bb, confidence)
-                    #person_detail[label_id].set_time(datetime.now())
-                    person_detail[label_id].set_time(timestamp + timedelta(seconds=(count // fps)))
-#           
-#                    if not person_detail[label_id].displayed:
-#                        pass
-#                        #person_detail[label_id].send_to_display(check_auth, office, floor)
-#                
-#            person_detail = remove_old_items(person_detail)
-#            print('*******************',datetime.now().strftime('%m/%d/%Y %I:%M:%S %p %Z'))
-#            print_details(person_detail)
+                    person_detail[label_id].assign_face(cropped_image[i], det, confidence)
+                    person_detail[label_id].set_time(timestamp + timedelta(seconds=(count // fps)))                
+                else:
+                    unknown_faceIds.append((f,cropped_image[i]))
+                    cv2.imwrite(f + '.jpg', cropped_image[i])
+#                    color = (255,0,255)
+#                    cv2.rectangle(frame, (det[0], det[1]), (det[2], det[3]), color, 2)
+       
         
-        
-        #print(face_box)
-        #print(person_detail)
         for person in person_detail.keys():
             if person in [j for i,j,k in face_box]:
                 person_detail[person].add_label(person)
             else:
                 person_detail[person].add_label(None)
-
         
-        #print('********************')        
+        #unknown_faceIds = unknown_faceIds[-3:]
+        unknown_face = {}
+        if len(unknown_faceIds) > 1:
+            get_face = [i for i, j in unknown_faceIds]
+            unknown_face = CF.face.group(get_face)
+            #print('\n',len(unknown_face['groups']),'\n')
+            print(unknown_face['groups'])
+
         # did anybody reach hits required
         for person in list(person_detail.keys()):
             #print('{0},{1}'.format(person, person_detail[person].counter))
-            
-
             if person_detail[person].counter == hits_required:
                 print('{1} Identified ----> {0}'.format(person, person_detail[person].time_keeper_fmt))
                 person_detail[person].reset()
             try:
-                #print (datetime.now().replace(tzinfo=tz.gettz('America/New_York')) - person_detail[person].display_time)
-
                 if timestamp + timedelta(seconds=(count // fps)) - person_detail[person].display_time > retention_time:
                     person_detail.pop(person, None)   
                     print('{1} Removed    ----> {0}'.format(person,  person_detail[person].time_keeper_fmt))
             except:
                 pass
-        #print('********************')        
+       
         (h, w) = frame.shape[:2]
         label_frame = np.zeros((h, label_size, 3), dtype=np.uint8)        
         picture_frame = np.zeros((h, picture_size, 3), dtype=np.uint8)
         decision_frame = np.zeros((h, decision_size, 3), dtype=np.uint8)
-        row_gap = 10
+        unknown_frame = np.zeros((h, unknown_size, 3), dtype=np.uint8)
         
-        for i, person in enumerate(person_detail.keys()):
+        label_frame.fill(255)
+        picture_frame.fill(255)
+        decision_frame.fill(255)
+        unknown_frame.fill(255)
+        
+        #row_gap = 10
+        
+        i = 0
+        
+        if unknown_face:
+            for a, grps in enumerate(unknown_face['groups']):
+                last_id = grps[-1] 
+                last_img = [y for x, y in unknown_faceIds if x == last_id][0]
+                y_offset = a * each_row_size
+                img_dis = cv2.resize(last_img,(each_row_size,unknown_size))
+                rows,cols,_ = img_dis.shape
+                unknown_frame[y_offset:y_offset + rows,:] = img_dis    
+            
+#        for x, y in enumerate(unknown_faceIds):
+#            id, face = y
+#            y_offset = x * each_row_size
+#            img_dis = cv2.resize(face,(each_row_size,unknown_size))
+#            rows,cols,_ = img_dis.shape
+#            unknown_frame[y_offset:y_offset + rows,:] = img_dis    
+            
+        for person in person_detail.keys():
+        
             if person_detail[person].display:
-                #cv2.putText(label_frame, str(person) , (label_size + i * label_size, label_size), cv2.FONT_HERSHEY_COMPLEX_SMALL,1, (255, 255, 255), thickness=1, lineType=2) 
-                cv2.putText(label_frame, str(person) , (0, i * (label_size + row_gap) + row_gap * 4), cv2.FONT_HERSHEY_COMPLEX_SMALL,1, (255, 255, 255), thickness=1, lineType=2) 
-                #rows,cols,_ = cv2.resize(person_detail[person].face_img.shape,(100,100))
-                img_dis = cv2.resize(person_detail[person].face_img,(int(picture_size * 0.75), int(picture_size * 0.75)))
+                #x_offset = 0 #row_gap
+                y_offset = i * each_row_size
+                decision_frame[y_offset:y_offset + each_row_size,:] = np.full([each_row_size,decision_size,3],[0,255,0],dtype=np.uint8)
+                
+                img_dis = cv2.resize(person_detail[person].face_img,(each_row_size,picture_size))
                 rows,cols,_ = img_dis.shape
-                #s_img = cv2.imread("smaller_image.png")
-                #l_img = cv2.imread("larger_image.jpg")
-                x_offset = row_gap
-                y_offset = i * (picture_size + row_gap) + row_gap 
-                ##label_frame[y_offset:y_offset+rows, x_offset:x_offset+cols] = person_detail[person].face_img
+                picture_frame[y_offset:y_offset + rows,:] = img_dis
                 
-                picture_frame[y_offset:y_offset+rows, x_offset: x_offset+cols] = img_dis
+                cv2.putText(label_frame, str(person) , (10, y_offset + each_row_size // 2 ), cv2.FONT_HERSHEY_COMPLEX_SMALL,1, (0, 0, 0), thickness=1, lineType=2) 
                 
-                
-                img_dis = np.full([int(decision_size * 0.75),int(decision_size * 0.75),3],[0,255,0],dtype=np.uint8)
-                rows,cols,_ = img_dis.shape
-                
-                x_offset = row_gap
-                y_offset = i * (decision_size + row_gap) + row_gap 
-                decision_frame[y_offset:y_offset+rows, x_offset: x_offset+cols] = img_dis
-                
-                
-                
-                #overlay=cv2.addWeighted(label_frame[row_gap:row_gap + i * rows, 0:0+cols],0.5,person_detail[person].face_img,0.5,0)
-
-                #label_frame[250:250+rows, 0:0+cols ] = overlay
-                
-                
-                #cv2.imshow('Image', np.hstack((picture_frame, label_frame, decision_frame)))
 
                 
                 
+#                cv2.putText(label_frame, str(person) , (0, i * (label_size + row_gap) + row_gap * 4), cv2.FONT_HERSHEY_COMPLEX_SMALL,1, (255, 255, 255), thickness=1, lineType=2) 
+#                img_dis = cv2.resize(person_detail[person].face_img,(int(picture_size * 1), int(picture_size * 1)))
+#                
+#                rows,cols,_ = img_dis.shape
+#
+#                x_offset = 0 #row_gap
+#                y_offset = i * (picture_size + row_gap) + row_gap 
+#                picture_frame[y_offset:y_offset+rows, x_offset: x_offset+cols] = img_dis
+#                
+#                
+#                color_img = np.full([rows,int(picture_size * decision_size),3],[0,255,0],dtype=np.uint8)
+#                rows,cols,_ = color_img.shape
+#                print(rows,cols)
+#                
+#                #img_dis = np.full([int(decision_size[0] * 0.75),int(decision_size[1] * 0.75),3],[0,255,0],dtype=np.uint8)
+#                #rows,cols,_ = img_dis.shape
+#                
+#                x_offset = row_gap
+#                y_offset = i * (int(picture_size * decision_size) + row_gap)  + row_gap
+#                print(x_offset,y_offset)
+#                decision_frame[y_offset:y_offset+rows, x_offset: x_offset+cols] = color_img
+                
+                i += 1
+                
+                
+                #cv2.imshow('Image', np.hstack((picture_frame, label_frame, decision_frame, unknown_frame)))
+
                 
         for det, lab, confi in face_box:
-            color = (0, 255,0)             
-#            if lab is None:
-#                color = (0, 0, 255)
-#            else:                
-#                color = (0, 255,0)
-#                text_x = det[0]
-#                text_y = det[3] + 20            
-#                #cv2.rectangle(frame, (det[0], det[1]), (det[2], det[3]), color, 2)
-#                cv2.putText(frame, str(lab) + ' ' + str(round(confi,2)) , (text_x, text_y), cv2.FONT_HERSHEY_COMPLEX_SMALL,1, (255, 255, 255), thickness=1, lineType=2)            
-
-            cv2.rectangle(frame, (det[0], det[1]), (det[2], det[3]), color, 2)
+            color = (255,0,0)             
+            if lab is None:
+                cv2.rectangle(frame, (det[0], det[1]), (det[2], det[3]), color, 2)        
+        #cv2.imshow('Image',cv2.resize(frame, (500,500)))
+        
+        final_frame = np.hstack((frame, decision_frame, picture_frame, label_frame, unknown_frame))
+        from win32api import GetSystemMetrics
+        width = GetSystemMetrics(0)
+        height = GetSystemMetrics(1)        
+        cv2.imshow("Show by CV2",cv2.resize(final_frame, (width-20,height-20)))
+        vout.write(final_frame)
+        
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            sys.exit()           
+        
+        count += 1
+        continue
             
-        #cv2.imshow('Image', frame)
-        
-        
-        
-        
-        
-        #height, width, depth = frame.shape
-        #W = 300
-        #imgScale = W/width
-        #newX,newY = frame.shape[1]*imgScale, frame.shape[0]*imgScale
-        #newimg = cv2.resize(frame,(int(newX),int(newY)))
-        #cv2.imshow("Show by CV2",newimg)
-        #print(frame.shape)
-        #print(label_frame.shape)
-        #cv2.imshow("Show by CV2",np.hstack((frame, decision_frame, picture_frame, label_frame)))
-        #vout.write(np.hstack((frame, decision_frame, picture_frame, label_frame)))
-                
-        #if cv2.waitKey(1) & 0xFF == ord('q'):
-        #    sys.exit()        
-        #time.sleep(1)
-        count += 1     
+
         
         
         
@@ -432,7 +438,7 @@ if __name__ == '__main__':
     if action == 'run':
         #pipe = create_pipe()
         #pipe = None
-        process_video_feed('input/1d4750c785cec00_KNC_6_DoorCamera_12347.mp4')
+        process_video_feed('input/1d4750c785cec00_KNC_6_DoorCamera_12349.mp4')
         #close_pipe(pipe)
 
     
